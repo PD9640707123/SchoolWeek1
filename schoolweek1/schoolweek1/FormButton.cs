7d3f6f37adfefb6ed5f794fff7070323496ec2d9
PD9640707123 <pd9640707@gmail.com>
2024-03-21 21:24:20 +0800

    First Commit

--- schoolweek1/schoolweek1/FormButton.cs
+++ schoolweek1/schoolweek1/FormButton.cs
@@ -32,5 +32,17 @@ namespace schoolweek1
             int sum = Int32.Parse(count)+1;
             label1.Text=sum.ToString();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String count = label1.Text;
+            int sum = Int32.Parse(count)-1;
+            label1.Text=sum.ToString();
+        }
     }
 }
